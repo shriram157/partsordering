a14258a57e5ae1bdf54c5dad9c37fcaf41dd84b6
Anthony Wat <anthony_wat@toyota.ca>
2019-03-27 11:12:49 -0400

    Standardized project structure and metadata

--- partsOrdering_node/lib/ProxyRequestHandler.js
+++ partsOrdering_node/lib/ProxyRequestHandler.js
@@ -7,8 +7,6 @@ var request = require('request');
 /*
  * hanlder/commander class - RequestHandler
  */
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 module.exports = class ProxyRequestHandler {
 
     constructor(apiUtil) {
