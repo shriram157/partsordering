78f483ca1bb151d4a6435d308f45a5542af1ba5b
Anthony Wat <anthony_wat@toyota.ca>
2019-03-27 13:30:07 -0400

    Replaced hardcoded client # with UPS config injected to proxied calls

--- partsOrdering_node/lib/ProxyRequestHandler.js
+++ partsOrdering_node/lib/ProxyRequestHandler.js
@@ -3,82 +3,86 @@
 
 var Boom = require('boom');
 var request = require('request');
-
+var {
+	URL
+} = require('url');
 /*
  * hanlder/commander class - RequestHandler
  */
 module.exports = class ProxyRequestHandler {
 
-    constructor(apiUtil) {
-        this._apiUtil = apiUtil;
-    }
-    
-    apiProxy(req, res, next, endpoint, logger){
-    	
-    	let self = this;    	
+	constructor(apiUtil) {
+		this._apiUtil = apiUtil;
+	}
 
-    	let headOptions = {};
-		
+	apiProxy(req, res, next, endpoint, logger) {
+
+		let self = this;
+
+		let headOptions = {};
+
 		// prepare the magic reverse proxy header for odata service to work.
-    	
-        let originalHost = req.hostname;
-        
-        // for the application to work with APIGEE gateway, has to remove the host header
-        if (!!req.headers.host){
-//            headOptions.host = req.headers.host;
+
+		let originalHost = req.hostname;
+
+		// for the application to work with APIGEE gateway, has to remove the host header
+		if (!!req.headers.host) {
+			//            headOptions.host = req.headers.host;
 			delete req.headers['host'];
-        }
+		}
 
-   //     if (!!req.headers['x-csrf-token']){
-   //         headOptions['x-csrf-token'] = req.headers['x-csrf-token'];
-   //     	headOptions['x-csrf-token'] = 'Fetch';
-			// delete req.headers['x-csrf-token'];
-   //     }
-        
-        if ('BASIC' ===  endpoint.authType) {
-        	let authHeader = 'Basic ' + new Buffer(endpoint.userName + ':' + endpoint.password ).toString('base64');
-        	headOptions.Authorization = authHeader;
-        } else if ('APIKEY' === endpoint.authType ){
-        	let authHeader = 'Basic ' + new Buffer(endpoint.userName + ':' + endpoint.password ).toString('base64');
-        	headOptions.Authorization = authHeader;
-        	headOptions.APIKey = endpoint.apiKey;
-        }
+		//     if (!!req.headers['x-csrf-token']){
+		//         headOptions['x-csrf-token'] = req.headers['x-csrf-token'];
+		//     	headOptions['x-csrf-token'] = 'Fetch';
+		// delete req.headers['x-csrf-token'];
+		//     }
 
-        let method = req.method;
-        let newUrl = req.url;
-        if (newUrl !== undefined && newUrl !== null){
-        	let iSub = newUrl.indexOf(endpoint.prefix);
-        	iSub = iSub + endpoint.prefix.length -1;
-        	if (iSub > 0 && iSub < newUrl.length  ){
-        		newUrl = newUrl.substr(iSub );
-        	}
-        }
+		if ('BASIC' === endpoint.authType) {
+			let authHeader = 'Basic ' + new Buffer(endpoint.userName + ':' + endpoint.password).toString('base64');
+			headOptions.Authorization = authHeader;
+		} else if ('APIKEY' === endpoint.authType) {
+			let authHeader = 'Basic ' + new Buffer(endpoint.userName + ':' + endpoint.password).toString('base64');
+			headOptions.Authorization = authHeader;
+			headOptions.APIKey = endpoint.apiKey;
+		}
+
+		let method = req.method;
+		let newUrl = req.url;
+		if (newUrl !== undefined && newUrl !== null) {
+			let iSub = newUrl.indexOf(endpoint.prefix);
+			iSub = iSub + endpoint.prefix.length - 1;
+			if (iSub > 0 && iSub < newUrl.length) {
+				newUrl = newUrl.substr(iSub);
+			}
+		}
 
 		let url = endpoint.proto + "://" + endpoint.host + ":" + endpoint.port + newUrl;
 
-		// if (method === 'POST' || method === 'PUT' || method === 'PATCH' || method ===
-		// } else if (method === 'GET' || method === 'HEADER' || method === 'DELETE' ) {
-		// }
+		// Add/update sap-client query parameter with UPS value in the proxied URL
+		if (endpoint.type === "odata") {
+			var urlObj = new URL(url);
+			urlObj.searchParams.delete("sap-client");
+			urlObj.searchParams.set("sap-client", endpoint.client);
+			url = urlObj.href;
+		}
 
-        let xRequest = 
+		let xRequest =
 			request({
-				method : method,
-				url : url,
+				method: method,
+				url: url,
 				headers: headOptions
- 			}
-		);
-        
-        req.pipe(xRequest);
-        
-        
-        xRequest.on('response', (response) => {
-        	// in order to make the csrfToken work
-        	//delete response.headers['set-cookie'];
-        	let csrfToken = response.headers['x-csrf-token'];
-        	xRequest.pipe(res);
-        }).on('error', (error) => {
-        	//let bErr = Boom.badGateway("error", error);
-        	next(Boom.badGateway("error", error));
-    	});   
-    }
+			});
+
+		req.pipe(xRequest);
+
+		xRequest.on('response', (response) => {
+			// in order to make the csrfToken work
+			//delete response.headers['set-cookie'];
+			let csrfToken = response.headers['x-csrf-token'];
+			xRequest.pipe(res);
+		}).on('error', (error) => {
+			//let bErr = Boom.badGateway("error", error);
+			next(Boom.badGateway("error", error));
+		});
+	}
 };
