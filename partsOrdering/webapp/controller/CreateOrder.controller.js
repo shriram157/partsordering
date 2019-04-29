92bb8c1d7f6d6731766b59774a0b5c329926db93
Ravi Harlalka <ravi_harlalka@toyota.ca>
2019-04-29 18:58:35 +0000

    Fix - Save Draft

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -530,7 +530,7 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 		},
 
 		onSaveDraft: function (oEvent) {
-			oEvent.getSource.setEnabled(false);
+			oEvent.getSource().setEnabled(false);
 			var that = this;
 			that.itemTable.setBusy(true);
 			that._oBusyDialog.setTitle("Save Draft");
@@ -563,7 +563,7 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 					}
 					that.toggleSubmitDraftButton();
 					that._oBusyfragment.close();
-					oEvent.getSource.setEnabled(true);
+					oEvent.getSource().setEnabled(true);
 				});
 			} else {
 				DataManager.saveDraftPurchaseOrder(this.aCreateItems, this.aUpdateItems, function (oPurchaseItem, sOperation, IIndex, isOK,
@@ -614,7 +614,7 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 					}
 					that.toggleSubmitDraftButton();
 					that._oBusyfragment.close();
-					oEvent.getSource.setEnabled(true);
+					oEvent.getSource().setEnabled(true);
 				});
 				this.toggleSubmitDraftButton();
 			}
