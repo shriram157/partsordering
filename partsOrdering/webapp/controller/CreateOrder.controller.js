98f0e3e02e4011ad2a1614d868df2afaec1470e9
Ravi Harlalka <ravi_harlalka@toyota.ca>
2019-05-21 17:22:01 +0000

    FR Language Issue Fix

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -393,8 +393,10 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 		handleSuggest: function (oEvent) {
 			var sTerm = oEvent.getParameter("suggestValue");
 			var aFilters = [];
+			var 
 			if (sTerm) {
 				aFilters.push(new sap.ui.model.Filter("Material", sap.ui.model.FilterOperator.Contains, sTerm));
+				aFilters.push(new sap.ui.model.Filter("LanguageKey", sap.ui.model.FilterOperator.EQ, this.sLang));
 			}
 			oEvent.getSource().getBinding("suggestionItems").filter(aFilters);
 		},
