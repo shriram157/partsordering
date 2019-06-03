18cd55f86d001c61d6187e40fabe57c597150407
Ravi Harlalka <ravi_harlalka@toyota.ca>
2019-06-03 18:32:27 +0000

    App Router

--- partsOrdering/webapp/controller/App.controller.js
+++ partsOrdering/webapp/controller/App.controller.js
@@ -7,6 +7,35 @@ sap.ui.define([
 
 	return BaseController.extend("tci.wave2.ui.parts.ordering.controller.App", {
 
-		onInit: function () {}
+		onInit: function () {
+			var that = this;
+
+			var resourceBundle = this.getResourceBundle();
+			var appStateModel = this.getAppStateModel();
+
+			// load the security based profile 
+			sap.ui.core.BusyIndicator.show(0);
+			this.getAppProfile(function (profileModel) {
+				sap.ui.core.BusyIndicator.hide();
+
+				if (!!profileModel) {
+					var profileModelData = profileModel.getData();
+					if (!!profileModelData.scopes.DealerBooking) {
+						appStateModel.setProperty('/visiblePA', false);
+						
+						that.getRouter().navTo("DealerBooking", {
+							encodedKey: "NEW"
+						}, true);
+						
+
+					} else {
+						appStateModel.setProperty('/visiblePA', true);
+						that.getRouter().navTo("SearchProgram", null, true);
+						
+					}
+
+			
+			
+		}
 	});
 });
