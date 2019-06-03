0cb6ed79f66310c4393d0e2b8b7b1e80a0ef566f
Ravi Harlalka <ravi_harlalka@toyota.ca>
2019-06-03 18:53:50 +0000

    Revert "App Router"
    This reverts commit 18cd55f86d001c61d6187e40fabe57c597150407.

--- partsOrdering/webapp/controller/App.controller.js
+++ partsOrdering/webapp/controller/App.controller.js
@@ -7,35 +7,6 @@ sap.ui.define([
 
 	return BaseController.extend("tci.wave2.ui.parts.ordering.controller.App", {
 
-		onInit: function () {
-			var that = this;
-
-			var resourceBundle = this.getResourceBundle();
-			var appStateModel = this.getAppStateModel();
-
-			// load the security based profile 
-			sap.ui.core.BusyIndicator.show(0);
-			this.getAppProfile(function (profileModel) {
-				sap.ui.core.BusyIndicator.hide();
-
-				if (!!profileModel) {
-					var profileModelData = profileModel.getData();
-					if (!!profileModelData.scopes.DealerBooking) {
-						appStateModel.setProperty('/visiblePA', false);
-						
-						that.getRouter().navTo("DealerBooking", {
-							encodedKey: "NEW"
-						}, true);
-						
-
-					} else {
-						appStateModel.setProperty('/visiblePA', true);
-						that.getRouter().navTo("SearchProgram", null, true);
-						
-					}
-
-			
-			
-		}
+		onInit: function () {}
 	});
 });
