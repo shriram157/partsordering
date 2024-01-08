31ae702226a15c339c282786f0b8c00fb01e7fd9
Shriram Sundaram <shriram_sundaram@toyota.ca>
2024-01-08 19:57:57 +0000

    trial and error

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -751,6 +751,71 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 			}
 		},
 
+		handleAddPart2: function (oEvent) {
+			var that = this;
+			var oSource = oEvent.getSource() || null; 
+			var oOrderData = this.getView().getModel("campaignModel").getData();
+			//this.itemTable.setBusy(true);
+			if (oSource) {
+				oSource.setEnabled(false);
+				oSource.setBusy(true);
+			}
+			var newAddedLineData = oOrderData.data[0];
+			
+			var oItem = JSON.parse(JSON.stringify(oOrderData.data[0]));
+			// oItem.addIcon = false;
+			// oItem.hasError = false;
+			// var lv_orderType = oItem.OrderType;
+			// oOrderData.items[0] = oItem;
+			// oOrderData.items[0].line = oOrderData.totalLines + 1;
+			// oOrderData.items[0].addIcon = false;
+			// oOrderData.items[0].selected = false;
+			// oOrderData.items[0]["ItemStatus"] = "Unsaved";
+			oOrderData.data.splice(oOrderData.data.length, 0, oOrderData.data[0]);
+			this.aCreateItems.push(oOrderData.data[0]);
+			// this.toggleSubmitDraftButton();
+			oOrderData.data.splice(0, 1);
+			var that = this;
+			//code by Minakshi for duplicate vin, ops, camp and partnum
+			// if (oOrderData.orderTypeId == 3) {
+			// 	oOrderData.items = oOrderData.items.reduce((unique, o) => {
+			// 		if (!unique.some(obj => obj.partNumber === o.partNumber && obj.campaignNum === o.campaignNum && obj.opCode === o.opCode && obj.vin ===
+			// 				o.vin)) {
+			// 			// var sInValid = that.oResourceBundle.getText("Create.Order.DuplicateCombination");
+			// 			// MessageBox.error(sInValid, {
+			// 			// 	actions: [MessageBox.Action.CLOSE],
+			// 			// 	styleClass: that.getOwnerComponent().getContentDensityClass()
+
+			// 			// });
+			// 			unique.push(o);
+			// 		}
+			// 		return unique;
+			// 	}, []);
+			// }
+
+			//code by Minakshi for duplicate vin, ops, camp and partnum
+
+			oOrderData.data.splice(0, 0, {
+					selected: false,
+					vinNum: " ",
+					CampaignCode: " ",
+					OperationCode: " "
+				});
+			//rData.newline = [that._getNewLine()];
+			// oOrderData.totalLines = oOrderData.items.length - 1;
+			// if (oOrderData.totalLines >= 16) {
+			// 	this.itemTable.setVisibleRowCount(oOrderData.items.length + 2);
+			// }
+			// ---to save some newwork traffic
+			// oOrderData.modifiedOn = new Date();
+			// that.oOrderModel.setData(oOrderData);campaignModel
+			this.getView().getModel("campaignModel").setData(oOrderData);
+			DataManager.setOrderData(oOrderData);
+			if (oSource) {
+				oSource.setEnabled(true);
+				oSource.setBusy(false);
+			}
+		},
 		//  Shriram changes for the DMND0004095
 		handleAddPart1: function (oEvent) {
 			var that = this;
