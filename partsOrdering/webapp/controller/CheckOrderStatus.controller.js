e6fc1398d8d8cae6d86930dca25f66924b810c61
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-01-11 08:42:11 +0000

    changes for DMND0003930

--- partsOrdering/webapp/controller/CheckOrderStatus.controller.js
+++ partsOrdering/webapp/controller/CheckOrderStatus.controller.js
@@ -661,22 +661,9 @@ sap.ui.define([
 				that._oDetailDialog.open();
 			});
 			//changes by Swetha for DMND003930 for close button validation
-			 var that = this;
 			 var bModel = this.getSalesOrderModel();
-			 var appStateModel = this.getStateModel();
-			 var dealerCode = appStateModel.getProperty('/selectedBP/dealerCode');
-			 var oFilter = new Array();
-			// //var dealerCode = conditions.dealerCode;
-			// var dealerCode = conditions.bpCode;
-
-			 oFilter[0] = new sap.ui.model.Filter("dealer_code", sap.ui.model.FilterOperator.EQ, dealerCode);
-			bModel.read('/find_soSet', {
-				 urlParameters: {
-				 	"$expand": "SOtoDeliv",
-				 	"$orderby": "TCI_order_no,TCI_itemNo"
-				 },
-				 filters: oFilter,
-				success: function (oData, oResponse) {
+			 bModel.read('/find_soSet', {
+				 success: function (oData, oResponse) {
 					if (!!oData && !!oData.results) {
 						if(oData.Flag!=""){
 							this.getView().byId("idClose").setVisible(true);
