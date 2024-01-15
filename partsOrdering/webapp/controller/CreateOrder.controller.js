a2ce78fd00a6a6ab771ea9e9eabf4a8f9522ca8a
Shriram Sundaram <shriram_sundaram@toyota.ca>
2024-01-15 15:47:01 +0000

    Reverted back changes

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -431,7 +431,8 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 			// 'Authorization': 'Basic',
 			// 	'x-ibm-client-secret': '10dd48f9-f090-4b06-bf4e-dc4d8817e25d',
 			// 	'x-ibm-client-id': 'j418Q~d2kIVPnOZ.dkhq0ENlrzbBFHuWk~oxqb_1',
-			var oURL = "/naqp/campaign/parts-details/v1";
+			// var oURL = "/naqp/campaign/parts-details/v1/";
+			   var oURL="TMNA/naqp/campaign/parts-details/v1/";
 			// var oURL="https://dev.api-int.naqp.toyota.com/naqp/campaign/parts-details/v1";
 			$.ajax({
 				type: 'POST',
