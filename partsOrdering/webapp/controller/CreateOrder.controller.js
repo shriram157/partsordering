ef14f01ed89963074f6638e9c1dee724140e52a0
Shriram Sundaram <shriram_sundaram@toyota.ca>
2024-01-17 11:18:27 +0000

    added forward slash

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -192,7 +192,7 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 					this._oDialog.open();
 					this.getView().byId("remSPQ").setVisible(false);
 					this.getView().byId("remComments").setVisible(false);
-					this.getView().getModel("stanrushModel").setProperty("typeCPOR",true);
+					this.getView().getModel("stanrushModel").setProperty("/typeCPOR",true);
 					// this.getView().getModel("orderModel").getData().typeCPOR = false;
 					// this.getView().getModel("orderModel").getData().typeCPOR1 = true;
 
