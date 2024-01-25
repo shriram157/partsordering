b4fdbd609cabad54ced0dbf381d8ae158fdde778
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-01-25 14:45:26 +0000

    changes for DMND0004095

--- partsOrdering/webapp/controller/FindOrder.controller.js
+++ partsOrdering/webapp/controller/FindOrder.controller.js
@@ -245,13 +245,13 @@ sap.ui.define([
 				this.getRouter().navTo("CreateOrder", {
 					orderNum: obj.uuid,
 					orderType: '-1',
-					CPORCB:sap.ui.getCore().getModel("cporModel").getData().cporCheckBox                             //changes by Swetha for DMND004095 on 25th Jan, 2024.
+					CPORCB:sap.ui.getCore().getModel("cporModel").oData                             //changes by Swetha for DMND004095 on 25th Jan, 2024.
 				});
 			} else {
 				this.getRouter().navTo("CreateOrder", {
 					orderNum: obj.orderNumber,
 					orderType: obj.scOrderType,
-					CPORCB:sap.ui.getCore().getModel("cporModel").getData().cporCheckBox 
+					CPORCB:sap.ui.getCore().getModel("cporModel").oData
 				});
 			}
 		},
