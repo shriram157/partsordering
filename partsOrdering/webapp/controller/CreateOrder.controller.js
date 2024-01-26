ec58efa28f7f1084f60e64bfc071c44e2e48db0c
Shriram Sundaram <shriram_sundaram@toyota.ca>
2024-01-26 11:15:31 +0000

    _fetchToken: function () {  enabled

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -492,7 +492,7 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 
 			var dataString = {
 				"campaignCode": "20TA02",
-				"vins": ["5TFUU4EN0DX068703", "5TFJU4GN1CX0", "JTMBD31V075106213", "5TFUU4EN0DX068703", "4T1B11HK4KU759632", "JTMP1RFV7KJ001307"]
+				"vins": ["5TFUU4EN0DX068703","5TFJU4GN1CX0", "JTMBD31V075106213", "5TFUU4EN0DX068703", "4T1B11HK4KU759632", "JTMP1RFV7KJ001307"]
 			};
 			// var oURL = "https://dev.api-int.naqp.toyota.com/naqp/campaign/parts-details/v1";
 			// 'Authorization': 'Basic',
