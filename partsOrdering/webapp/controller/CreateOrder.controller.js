7b7835cc5b0915981edaf95b22acd5d4ebce42c9
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-02-12 15:44:59 +0000

    changes for DMND0004095

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -650,7 +650,8 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 								"qty": item.parts[0].maxQty,
 								"partNumber": item.parts[0].partNumber,
 								"partDesc": item.parts[0].partDescription,
-								"line": that.getView().getModel("orderModel").oData.totalLines + 1
+								"line": that.getView().getModel("orderModel").oData.totalLines + 1,
+								"hasError":false
 
 								//	"OperationCode": that.getView().getModel("campaignModel").oData.data[i].OperationCode
 
