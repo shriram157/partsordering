1ddab7e4047db15e0f1b632407ff3e816bf1427d
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-02-14 11:50:57 +0000

    changes for DMND0004095

--- partsOrdering/webapp/controller/CreateOrder.controller.js
+++ partsOrdering/webapp/controller/CreateOrder.controller.js
@@ -2036,25 +2036,25 @@ sap.ui.define(["tci/wave2/ui/parts/ordering/controller/BaseController", 'sap/m/M
 			if (obj.addIcon !== true) {
 				var newValue = oEvent.getParameter("newValue");
 				if (newValue > 0) {
-					if (this.getView().getModel("orderModel").oData.typeCPOR == true) { //changes by swetha for DMND0004095
-						var oRow = oEvent.getSource().getParent(); //Get Row
-						var oTable = oRow.getParent(); // Get Table
-						var iRowIndex = oTable.indexOfRow(oRow); //Get Row index
-						var maxqty = this.getView().getModel("orderModel").oData.items[iRowIndex].qty;
-						var qtyMsg = that.oResourceBundle.getText("QtyCheck") + ' ' + maxqty;
-						if (newValue > maxqty) {
-							sap.m.MessageBox.show(qtyMsg, {
-								icon: MessageBox.Icon.ERROR,
-								title: that.oResourceBundle.getText("ERROR"),
-								actions: [MessageBox.Action.OK],
-								onClose: function (sAction) {
-									if (sAction == "OK") {
-										oEvent.getSource().setValue(maxqty);
-									}
-								}
-							});
-						}
-					}
+					// if (this.getView().getModel("orderModel").oData.typeCPOR == true) { //changes by swetha for DMND0004095
+					// 	var oRow = oEvent.getSource().getParent(); //Get Row
+					// 	var oTable = oRow.getParent(); // Get Table
+					// 	var iRowIndex = oTable.indexOfRow(oRow); //Get Row index
+					// 	var maxqty = this.getView().getModel("orderModel").oData.items[iRowIndex].qty;
+					// 	var qtyMsg = that.oResourceBundle.getText("QtyCheck") + ' ' + maxqty;
+					// 	if (newValue > maxqty) {
+					// 		sap.m.MessageBox.show(qtyMsg, {
+					// 			icon: MessageBox.Icon.ERROR,
+					// 			title: that.oResourceBundle.getText("ERROR"),
+					// 			actions: [MessageBox.Action.OK],
+					// 			onClose: function (sAction) {
+					// 				if (sAction == "OK") {
+					// 					this.getView().getModel("orderModel").oData.items[iRowIndex].qty;	
+					// 				}
+					// 			}
+					// 		});
+					// 	}
+					// }
 					obj.ItemStatus = "Unsaved";
 
 					oSource.setValueStateText("");
