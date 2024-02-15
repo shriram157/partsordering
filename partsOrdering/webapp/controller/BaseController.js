5af3fc770cf3f811cd77f743062b5f0b5dac5642
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-02-15 17:56:29 +0000

    changes reverted

--- partsOrdering/webapp/controller/BaseController.js
+++ partsOrdering/webapp/controller/BaseController.js
@@ -739,8 +739,7 @@ sap.ui.define([
 			var key = bModel.createKey('/C_Product', {
 				'Product': material,
 				"DraftUUID": DraftId,
-				"IsActiveEntity":true,
-				"Language":lan                                    //changes by Swetha for DMND0003930 on 15th Feb, 2024 added Language
+				"IsActiveEntity":true
 			});
 			bModel.read(key, {
 				urlParameters: {},
