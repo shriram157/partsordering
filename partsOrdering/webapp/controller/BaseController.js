3f0c71e6914bd29136089005d1afc9e160b7761b
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-02-20 17:34:35 +0000

    changes reverted

--- partsOrdering/webapp/controller/BaseController.js
+++ partsOrdering/webapp/controller/BaseController.js
@@ -1639,12 +1639,7 @@ sap.ui.define([
 						lv_orderData.orderTypeId = that.getInnerOrderTypeByZOrderType(lv_orderData.zOrderType);
 						lv_orderData.orderTypeName = that.getOrderTypeName(orderData.orderTypeId);
 						lv_orderData.tciOrderNumber = oData.results[0].PurchNoC;
-						lv_orderData.zzcpor = oData.zzcpor;                                                //changes by swetha for DMND0004095 on 20th Feb, 2024
-						if(lv_orderData.zzcpor=="X"){
-							sap.ui.getCore().getModel("CPORModel").setData("true");
-						} else {
-							sap.ui.getCore().getModel("CPORModel").setData("false");	
-						}
+						
 
 						//lv_draft.dealerCode = lv_aResult.ZZ1_DealerCode_PDH;
 						lv_orderData.bpCode = oData.results[0].SoldtoParty;
@@ -1739,12 +1734,7 @@ sap.ui.define([
 						lv_orderData.orderTypeId = that.getInnerOrderTypeByZOrderType(lv_orderData.zOrderType);
 						lv_orderData.orderTypeName = that.getOrderTypeName(orderData.orderTypeId);
 						lv_orderData.tciOrderNumber = oData.PurchNoC;
-						lv_orderData.zzcpor = oData.zzcpor; //changes by swetha for DMND0004095 on 20th Feb, 2024
-						if (lv_orderData.zzcpor == "X") {
-							sap.ui.getCore().getModel("CPORModel").setData("true");
-						} else {
-							sap.ui.getCore().getModel("CPORModel").setData("false");
-						}
+						
 						//lv_draft.dealerCode = lv_aResult.ZZ1_DealerCode_PDH;
 						lv_orderData.bpCode = oData.SoldtoParty;
 
