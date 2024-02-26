865b25e81495830b47153c13b0f52a8cbabd2b23
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2024-02-26 16:47:03 +0000

    changes for DMND0004095

--- partsOrdering/webapp/controller/BaseController.js
+++ partsOrdering/webapp/controller/BaseController.js
@@ -1848,7 +1848,7 @@ sap.ui.define([
 				'VIN_no': vinNo,
 				'part_no': partNum
 			});
-		//	for (var i = 0; i < this.getView().getModel("orderModel").getData().items.length; i++) {
+			for (var i = 0; i < this.getView().getModel("orderModel").getData().items.length; i++) {
 				bModel.read(key, {
 						success: function (oData, oResponse) {
 							var messageList = that._extractSapItemMessages(oResponse);
@@ -1865,13 +1865,13 @@ sap.ui.define([
 							var errorResponse = JSON.parse(oError.responseText);
 							var errMessage = errorResponse.error.message.value;
 							var orderData = that.getView().getModel("orderModel").getData();
-							orderData.items.errMessage = errMessage;
+							orderData.items[i].errMessage = errMessage;
 							orderModel.setData(orderData);
 							callbackFn(errMessage, false);
 							//callback(null, false, []);
 						}
 					});
-			//	}
+				}
 		},
 
 		/*	// only for sale order 		// Moved To DataManager	
